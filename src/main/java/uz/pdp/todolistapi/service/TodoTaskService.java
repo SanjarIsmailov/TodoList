a99a1f8a06178b325da9a58a1c56752571988a3a
SanjarPARSIFAL <sanjar666parsifal@gmail.com>
2024-07-18 20:59:28 +0500

    finished

--- src/main/java/uz/pdp/todolistapi/service/TodoTaskService.java
+++ src/main/java/uz/pdp/todolistapi/service/TodoTaskService.java
@@ -1,7 +1,6 @@
 package uz.pdp.todolistapi.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import uz.pdp.todolistapi.entity.TodoTask;
